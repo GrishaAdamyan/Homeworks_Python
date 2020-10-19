a41418562c6558ba3db032913803be3fcc9ad628
GrishaAdamyan <g.adamyan2004@gmail.com>
2020-10-20 01:21:06 +0400

    3rd dasi tnayin ashxatanq

--- homework_3.py
+++ homework_3.py
@@ -1,17 +1,16 @@
 # 1
-#n = int(input())
-#list1 = []
-#for i in range(n):
-    #name = input("name: ")
-    #mark = float(input("mark: "))
-    #list1.append([mark,name])
-#list1.sort()
-#list2 = [list1[j][0] for j in range(n)]
-#low = list2.count(list2[0])
-#i = low
-#while i <= len(list1)-1 and list2[i] == list2[low]:
-    #print(list1[i][1])
-    #i += 1
+n = int(input())
+list1 = []
+for i in range(n):
+    name = input("name: ")
+    mark = float(input("mark: "))
+    list1.append([mark,name])
+list1.sort()
+low = [list1[j][0] for j in range(n)].count([list1[j][0] for j in range(n)][0])
+i = low
+while i <= len(list1)-1 and [list1[j][0] for j in range(n)][i] == [list1[j][0] for j in range(n)][low]:
+    print(list1[i][1])
+    i += 1
 
 # 2
 count = 0
