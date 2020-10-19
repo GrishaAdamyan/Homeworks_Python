cf588cdf4ba26c9856800612d872a9148a92100f
GrishaAdamyan <g.adamyan2004@gmail.com>
2020-10-20 00:55:55 +0400

    3rd dasi tnayin ashxatanq

--- homework_3.py
+++ homework_3.py
@@ -1,14 +1,33 @@
 # 1
-n = int(input())
+#n = int(input())
+#list1 = []
+#for i in range(n):
+    #name = input("name: ")
+    #mark = float(input("mark: "))
+    #list1.append([mark,name])
+#list1.sort()
+#list2 = [list1[j][0] for j in range(n)]
+#low = list2.count(list2[0])
+#i = low
+#while i <= len(list1)-1 and list2[i] == list2[low]:
+    #print(list1[i][1])
+    #i += 1
+
+# 2
+count = 0
+superString = input()
+subString = input()
 list1 = []
-for i in range(n):
-    name = input("name: ")
-    mark = float(input("mark: "))
-    list1.append([mark,name])
+string = ''
+for i in range(len(superString)):
+    if superString[i] in subString and len(subString) - count == 1:
+        string += superString[i]
+        count = 0
+        list1.append(string)
+    elif superString[i] in subString:
+        string += superString[i]
+        count += 1
+    else:
+        string += superString[i]
 list1.sort()
-list2 = [list1[j][0] for j in range(n)]
-low = list2.count(list2[0])
-i = low
-while i <= len(list1)-1 and list2[i] == list2[low]:
-    print(list1[i][1])
-    i += 1
+print(list1[0])
