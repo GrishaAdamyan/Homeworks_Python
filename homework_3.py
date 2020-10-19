64945bcc29a3143309900b26ea45c556233aeaa7
GrishaAdamyan <g.adamyan2004@gmail.com>
2020-10-20 00:19:36 +0400

    3rd dasi arajin xndir:

--- homework_3.py
+++ homework_3.py
@@ -5,10 +5,8 @@ for i in range(n):
     name = input("name: ")
     mark = float(input("mark: "))
     list1.append([mark,name])
-list2 = []
-for j in range(n):
-    list2.append(list1[j][0])
-list2.sort()
+list1.sort()
+list2 = [list1[j][0] for j in range(n)]
 low = list2.count(list2[0])
 i = low
 while i <= len(list1)-1 and list2[i] == list2[low]:
