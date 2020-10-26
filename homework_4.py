d61cbc97feecda041755ccbb6e1e4eb6c8b87043
GrishaAdamyan <g.adamyan2004@gmail.com>
2020-10-26 23:11:17 +0400

    4rd dasi tnayin ashxatanq

--- homework_4.py
+++ homework_4.py
@@ -1,110 +1,25 @@
 # 1
-matrix = [['True', 'False', 'False'], 
-          ['False', 'True', 'False'],
-          ['False', 'False', 'False']]
-minesweeper_matrix = [[],[],[]]
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        count = 0
-        if i == 0 and j == 0:
-            if matrix[i+1][j] == 'True':
-                count += 1
-            if matrix[i][j+1] == 'True':
-                count += 1
-            if matrix[i+1][j+1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i == 0 and j != 0 and j != len(matrix[i])-1:
-            if matrix[i][j-1] == 'True':
-                count += 1
-            if matrix[i+1][j] == 'True':
-                count += 1
-            if matrix[i+1][j-1] == 'True':
-                count += 1
-            if matrix[i][j+1] == 'True':
-                count += 1
-            if matrix[i+1][j+1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i == 0 and j == len(matrix[i])-1:
-            if matrix[i][j-1] == 'True':
-                count += 1
-            if matrix[i+1][j] == 'True':
-                count += 1
-            if matrix[i+1][j-1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i != 0 and i != len(matrix)-1 and j == 0:
-            if matrix[i-1][j] == 'True':
-                count += 1
-            if matrix[i-1][j+1] == 'True':
-                count += 1
-            if matrix[i][j+1] == 'True':
-                count += 1
-            if matrix[i+1][j] == 'True':
-                count += 1
-            if matrix[i+1][j+1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i != 0 and i != len(matrix)-1 and j != 0 and j != len(matrix[i])-1:
-            if matrix[i-1][j] == 'True':
-                count += 1
-            if matrix[i+1][j] == 'True':
-                count += 1
-            if matrix[i][j-1] == 'True':
-                count += 1
-            if matrix[i][j+1] == 'True':
-                count += 1
-            if matrix[i-1][j-1] == 'True':
-                count += 1
-            if matrix[i+1][j+1] == 'True':
-                count += 1
-            if matrix[i-1][j+1] == 'True':
-                count += 1
-            if matrix[i+1][j-1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i != 0 and i != len(matrix)-1 and j == len(matrix[i])-1:
-            if matrix[i-1][j] == 'True':
-                count += 1
-            if matrix[i-1][j-1] == 'True':
-                count += 1
-            if matrix[i][j-1] == 'True':
-                count += 1
-            if matrix[i+1][j] == 'True':
-                count += 1
-            if matrix[i+1][j-1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i == len(matrix)-1 and j == 0:
-            if matrix[i-1][j] == 'True':
-                count += 1
-            if matrix[i-1][j+1] == 'True':
-                count += 1
-            if matrix[i][j+1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i == len(matrix)-1 and j != 0 and j != len(matrix)-1:
-            if matrix[i][j-1] == 'True':
-                count += 1
-            if matrix[i-1][j] == 'True':
-                count += 1
-            if matrix[i-1][j-1] == 'True':
-                count += 1
-            if matrix[i][j+1] == 'True':
-                count += 1
-            if matrix[i-1][j+1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-        elif i == len(matrix)-1 and j == len(matrix)-1:
-            if matrix[i-1][j] == 'True':
-                count += 1
-            if matrix[i-1][j-1] == 'True':
-                count += 1
-            if matrix[i][j-1] == 'True':
-                count += 1
-            minesweeper_matrix[i].append(count)
-print(minesweeper_matrix)
+def get_neighbours_coords(i_cord, j_cord, matrix_i, matrix_j):
+    coords = []
+    for i in range(i_cord - 1, i_cord + 2):
+        for j in range(j_cord - 1, j_cord + 2):
+            if 0 <= i <= matrix_i - 1 and 0 <= j <= matrix_j - 1 and (i, j) != (i_cord, j_cord):
+                coords.append((i, j))
+    return coords
+
+
+def minesweeper(matrix):
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            neighbours_coords = get_neighbours_coords(i, j, len(matrix), len(matrix[i]))
+            count = 0
+            for coord_x, coord_y in neighbours_coords:
+                if matrix[coord_x][coord_y]:
+                    count += 1
+            new_matrix[i].append(count)
+    return new_matrix
 
 
 # 2
