626d7e9473ef674e8d239766f5447d01747c586d
GrishaAdamyan <g.adamyan2004@gmail.com>
2020-11-07 00:42:10 +0400

    8rd dasi tnayin ashxatanq

--- homework_8.py
+++ homework_8.py
@@ -1,9 +1,23 @@
 # 1
 def answer_queries(k, *query_counts):
-    return (sum(query_counts) // k) + 1
+    count = 1
+    list1 = list(query_counts)
+    for i in range(len(list1)):
+        if i != len(list1) - 1:
+            if k > list1[i]:
+                return count
+            elif k < list1[i]:
+                list1[i + 1] += list1[i] - k
+            count += 1
+        else:
+            if k > list1[i]:
+                return count
+            elif k < list1[i]:
+                count += (list1[i]) // k
+                return count
 
 
-print(answer_queries(1, 100))
+print(answer_queries(5, 10, 5, 5, 3, 2, 1))
 
 # 2
 def non_decreasing_sequence(*nums):
@@ -11,7 +25,7 @@ def non_decreasing_sequence(*nums):
     for i in range(len(list1)):
         list1[i] = 0 - list1[i]
         if list1 == sorted(list1):
-            return 'Yes'
+            return 'Yes', list1
         else:
             list1[i] = 0 - list1[i]
     for i in range(len(list1)):
@@ -19,11 +33,11 @@ def non_decreasing_sequence(*nums):
             list1[i] = 0 - list1[i]
             list1[j] = 0 - list1[j]
             if list1 == sorted(list1):
-                return 'Yes'
+                return 'Yes', list1
             else:
                 list1[i] = 0 - list1[i]
                 list1[j] = 0 - list1[j]
     return 'No'
 
 
-print(non_decreasing_sequence(1, 1, 1))
+print(non_decreasing_sequence(1, 1, 0))
