3ca4f615c749cc69fb499f61b026bf5be45a637a
GrishaAdamyan <g.adamyan2004@gmail.com>
2020-12-22 15:47:18 +0400

    19rd dasi tnayin ashxatanq

--- homework_19.py
+++ homework_19.py
@@ -10,9 +10,9 @@ class Weapon:
             second = target.get_coords()
             h = ((first[0] - second[0]) ** 2 + (first[1] - second[1]) ** 2) ** 0.5
             if h > self.range:
-                print(f'target is too far for weapon {name}')
+                print(f'target is too far for weapon {self.name}')
             else:
-                print(f'enemy was hit from weapon {name}, damage is {damage}')
+                print(f'enemy was hit from weapon {self.name}, damage is {self.damage}')
                 target.get_damage(self.damage)
         else:
             print("the enemy is already defeated")
@@ -25,8 +25,8 @@ class BaseCharacter:
         self.pos_y = pos_y
         self.hp = hp
     def move(self, delta_x, delta_y):
-        self.pos_x = delta_x
-        self.pos_y = delta_y
+        self.pos_x += delta_x
+        self.pos_y += delta_y
     def is_alive(self):
         return self.hp != 0
     def get_damage(self, amount):
@@ -59,7 +59,7 @@ class MainHero(BaseCharacter):
             if len(self.weapon) == 0:
                 print('I am unarmed')
             elif isinstance(target, BaseEnemy):
-                self.weapon.hit(self, target)
+                self.weapon[0].hit(self, target)
         else:
             print('I can hit only enemy')
     def add_weapon(self, weapon):
@@ -82,3 +82,27 @@ class MainHero(BaseCharacter):
         else:
             self.hp += amount
         print(f'now my health is {self.hp}')
+
+
+
+weapon1 = Weapon("Короткий меч", 5, 1)
+weapon2 = Weapon("Длинный меч", 7, 2)
+weapon3 = Weapon("Лук", 3, 10)
+weapon4 = Weapon("Лазерная орбитальная пушка", 1000, 1000)
+princess = BaseCharacter(100, 100, 100)
+archer = BaseEnemy(50, 50, weapon3, 100)
+armored_swordsman = BaseEnemy(10, 10, weapon2, 500)
+archer.hit(armored_swordsman)
+armored_swordsman.move(10, 10)
+print(armored_swordsman.get_coords())
+main_hero = MainHero(0, 0, "Король Артур", 200)
+main_hero.hit(armored_swordsman)
+main_hero.next_weapon()
+main_hero.add_weapon(weapon1)
+main_hero.hit(armored_swordsman)
+main_hero.add_weapon(weapon4)
+main_hero.hit(armored_swordsman)
+main_hero.next_weapon()
+main_hero.hit(princess)
+main_hero.hit(armored_swordsman)
+main_hero.hit(armored_swordsman)
